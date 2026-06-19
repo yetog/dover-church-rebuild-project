deef2703e1fd728be6aecc184c5aa90197761477
Isayah Young-Burke <isayahy@gmail.com>
2026-06-19 14:01:58 -0400

    feat: add routes for all new stub pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,15 @@ import Sermons from "./pages/Sermons";
 import Meditation from "./pages/Meditation";
 import Newsletter from "./pages/Newsletter";
 import Contact from "./pages/Contact";
+import UCC from "./pages/UCC";
+import Staff from "./pages/Staff";
+import Partners from "./pages/Partners";
+import Happening from "./pages/Happening";
+import Community from "./pages/Community";
+import ChildrenYouth from "./pages/ChildrenYouth";
+import Neighbors from "./pages/Neighbors";
+import CommunityHealth from "./pages/CommunityHealth";
+import HelpingOthers from "./pages/HelpingOthers";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -26,7 +35,15 @@ const App = () => (
           <Route path="/meditation" element={<Meditation />} />
           <Route path="/newsletter" element={<Newsletter />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/ucc" element={<UCC />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/partners" element={<Partners />} />
+          <Route path="/happening" element={<Happening />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/community/children-youth" element={<ChildrenYouth />} />
+          <Route path="/community/neighbors" element={<Neighbors />} />
+          <Route path="/community/health" element={<CommunityHealth />} />
+          <Route path="/helping-others" element={<HelpingOthers />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
